02f35c0457af48d2bf1baf84f1f52369b96c55d7
Jan Szczuryk <jan.szczuryk@gmail.com>
2022-11-13 16:26:55 +0100

    Added volume to docker-compose. Fixed scripts

--- scripts/db-schema.sh
+++ scripts/db-schema.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Create database schema ..."
 
-source ../.env
+DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+source "$DIR/../.env"
 docker exec -i dbp-mariadb-container mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" -D "$MYSQL_DATABASE" < ./sql/schema-create.sql
